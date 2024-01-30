71e8763744eb6eff953ab22114978af291fab2a6
Alicia Mendez Cruz <aliciamendezcruz@alichus-mbp-16.home>
2024-01-30 12:07:57 +0100

    start of program

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# ch16-weather-app-02
+weather app form vanilla turned into a react based app
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.scss";
+
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <>
+        <p>Weather App</p>
+      </>
+    );
+  }
+}
+
+export default App;
